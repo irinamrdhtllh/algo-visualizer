f98b3d929c21ae2c670e9d41732662a448fadbe9
Irina M. S <irinamardhatillah@gmail.com>
2024-12-02 12:12:45 +0700

    Tidy up the codes

---
+++ main.js
@@ -0,0 +1,59 @@
+import { insertionSort, mergeSort } from "./algorithms.js";
+
+const randomButton = document.getElementById("random");
+const runButton = document.getElementById("run");
+const container = document.getElementById("container");
+let nums = [];
+
+function generateArray(size = 50) {
+    nums = [];
+    for (let i = 0; i < size; i++) {
+        nums.push(Math.floor(Math.random() * 20) + 1);
+    }
+    render();
+}
+
+function render(focusIndex) {
+    container.innerHTML = "";
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const div = document.createElement("div");
+        div.style.width = "42px";
+        div.style.height = `${18 * num}px`;
+        div.style.border = "1px solid black";
+        if (i == focusIndex) {
+        div.style.background = "purple";
+        }
+        container.append(div);
+    }
+}
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+randomButton.addEventListener("click", () => {
+    generateArray(10);
+});
+
+runButton.addEventListener("click", () => {
+    const selectedAlgorithm =
+        document.getElementById("algorithm-list").value;
+
+    switch (selectedAlgorithm) {
+        case "Insertion Sort":
+        insertionSort(nums, async (focusIndex) => {
+            render(focusIndex);
+            await delay(100);
+        });
+        break;
+        case "Merge Sort":
+        mergeSort(nums, async (focusIndex) => {
+            render(focusIndex);
+            await delay(100);
+        });
+        break;
+    }
+});
+
+generateArray(10);
